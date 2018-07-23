090f7cf4e9666270db0167722848b5556283ae8c
slb <782125244@qq.com>
2018-07-23 10:08:49 +0800

    动态SQL（choose、set、foreach标签、in查询、mysql和oracle批量保存员工信息）

--- mybatis/src/main/java/com/atguigu/mybatis/dao/EmployeeMapperDynamicSQL.java
+++ mybatis/src/main/java/com/atguigu/mybatis/dao/EmployeeMapperDynamicSQL.java
@@ -1,6 +1,7 @@
 package com.atguigu.mybatis.dao;
 
 import com.atguigu.mybatis.bean.Employee;
+import org.apache.ibatis.annotations.Param;
 
 import java.util.List;
 
@@ -12,5 +13,19 @@ public interface EmployeeMapperDynamicSQL {
 
     //动态查询条件，测试trim
     public List<Employee> getEmpsByConditionTrim(Employee employee);
+
+    //动态查询条件，测试choose
+    public List<Employee> getEmpsByConditionChoose(Employee employee);
+
+    //动态修改条件，更新员工
+    public void updateEmp(Employee employee);
+
+    //根据封装好的多个员工id查询，结果集封装在给定集合中的
+    public List<Employee> getEmpsByConditionForeach(@Param("ids")List<Integer> ids);
+
+    //批量保存员工
+    public void addEmps(@Param("emps") List<Employee> employees);
+
+
 
 }
--- mybatis/src/main/java/com/atguigu/mybatis/test/MyBatisDynamicSQLTest.java
+++ mybatis/src/main/java/com/atguigu/mybatis/test/MyBatisDynamicSQLTest.java
@@ -1,5 +1,6 @@
 package com.atguigu.mybatis.test;
 
+import com.atguigu.mybatis.bean.Department;
 import com.atguigu.mybatis.bean.Employee;
 import com.atguigu.mybatis.dao.EmployeeMapperDynamicSQL;
 import org.apache.ibatis.io.Resources;
@@ -10,6 +11,8 @@ import org.junit.Test;
 
 import java.io.IOException;
 import java.io.InputStream;
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 public class MyBatisDynamicSQLTest {
@@ -68,4 +71,97 @@ public class MyBatisDynamicSQLTest {
             sqlSession.close();
         }
     }
+
+    //根据动态查询条件查询员工信息,使用choose
+    @Test
+    public void test03() throws IOException {
+        //获得sqlSessionfactory工厂
+        SqlSessionFactory sqlSessionFactory = getSqlSessionFactory();
+        //获得sqlSession
+        SqlSession sqlSession = sqlSessionFactory.openSession();
+        try {
+            //获取接口的实现类对象,会为接口自动的创建一个代理对象，代理对象去执行增删改查方法
+            EmployeeMapperDynamicSQL mapper = sqlSession.getMapper(EmployeeMapperDynamicSQL.class);
+            Employee employee = new Employee(1, null, null, null);
+            //查询
+            List<Employee> employees = mapper.getEmpsByConditionChoose(employee);
+            for (Employee emp : employees) {
+                System.out.println(emp);
+            }
+        } catch (Exception ex) {
+            ex.printStackTrace();
+        } finally {
+            sqlSession.close();
+        }
+    }
+
+
+    //动态修改条件，更新员工，测试set标签
+    @Test
+    public void test04() throws IOException {
+        //获得sqlSessionfactory工厂
+        SqlSessionFactory sqlSessionFactory = getSqlSessionFactory();
+        //获得sqlSession
+        SqlSession sqlSession = sqlSessionFactory.openSession();
+        try {
+            //获取接口的实现类对象,会为接口自动的创建一个代理对象，代理对象去执行增删改查方法
+            EmployeeMapperDynamicSQL mapper = sqlSession.getMapper(EmployeeMapperDynamicSQL.class);
+            Employee employee = new Employee(1, "jerry", null, null);
+            //更新
+            mapper.updateEmp(employee);
+
+            //提交
+            sqlSession.commit();
+        } catch (Exception ex) {
+            ex.printStackTrace();
+        } finally {
+            sqlSession.close();
+        }
+    }
+
+
+    //根据封装好的多个员工id查询，结果集封装在给定集合中的
+    @Test
+    public void test05() throws IOException {
+        //获得sqlSessionfactory工厂
+        SqlSessionFactory sqlSessionFactory = getSqlSessionFactory();
+        //获得sqlSession
+        SqlSession sqlSession = sqlSessionFactory.openSession();
+        try {
+            //获取接口的实现类对象,会为接口自动的创建一个代理对象，代理对象去执行增删改查方法
+            EmployeeMapperDynamicSQL mapper = sqlSession.getMapper(EmployeeMapperDynamicSQL.class);
+            //查询
+            List<Employee> employees = mapper.getEmpsByConditionForeach(Arrays.asList(1, 4));
+            for (Employee emp : employees) {
+                System.out.println(emp);
+            }
+        } catch (Exception ex) {
+            ex.printStackTrace();
+        } finally {
+            sqlSession.close();
+        }
+    }
+
+    //批量保存员工
+    @Test
+    public void testBatchSave() throws IOException {
+        //获得sqlSessionfactory工厂
+        SqlSessionFactory sqlSessionFactory = getSqlSessionFactory();
+        //获得sqlSession
+        SqlSession sqlSession = sqlSessionFactory.openSession();
+        try {
+            //获取接口的实现类对象,会为接口自动的创建一个代理对象，代理对象去执行增删改查方法
+            EmployeeMapperDynamicSQL mapper = sqlSession.getMapper(EmployeeMapperDynamicSQL.class);
+            ArrayList<Employee> employees = new ArrayList<>();
+            employees.add(new Employee(null, "smith", "smith@guigu.com", "1", new Department(1)));
+            employees.add(new Employee(null, "allen", "allen@guigu.com", "0", new Department(1)));
+            mapper.addEmps(employees);
+            sqlSession.commit();
+        } catch (Exception ex) {
+            ex.printStackTrace();
+        } finally {
+            sqlSession.close();
+        }
+    }
+
 }
